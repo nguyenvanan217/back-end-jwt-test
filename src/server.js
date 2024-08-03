9e591d2e99c952dfacbc1303712c79fc0b050a00
An Nguyen <annguyenmxh@gmail.com>
2024-08-03 17:07:17 +0700

    push

--- src/server.js
+++ src/server.js
@@ -2,15 +2,18 @@ import express from "express";
 import bodyParser from "body-parser"; //lấý dữ liệu như id , dùng query param
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./Routes/web";
+import Connection from "./configs/connectdb";
+
 require("dotenv").config();
 
 let app = express();
 //config app
+configViewEngine(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+//test Connection db
+Connection();
 initWebRoutes(app);
-configViewEngine(app)
-
 let port = process.env.PORT || 6969;
 app.listen(port, () => {
   console.log("App is running at the port: " + port);
