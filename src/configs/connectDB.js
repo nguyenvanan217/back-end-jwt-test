fd130b79df237618f17acd9207ed8b698c56b9b0
An Nguyen <annguyen828721665@gmail.com>
2024-11-22 10:12:38 +0700

    first commit

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('thuchanhfull', 'root', null, {
+const sequelize = new Sequelize('testJWT', 'root', null, {
   host: 'localhost',
   dialect: 'mysql'
 });
